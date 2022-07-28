c01de6d16191986c68233f8efc35d4410189e137
Amy Chen <amysbubble@Amys-MacBook-Pro-2.local>
2022-07-28 14:34:57 -0700

    disable strict mode that caused useEffect to be called twice

--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ import App from './components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // turn strict mode off as it causes useEffect to be called twice,
+  // see issue https://github.com/facebook/react/issues/24455
+  // <React.StrictMode>
+  <App />
+  // </React.StrictMode>
 );
 
