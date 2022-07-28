874d346ae65c3ea94b5eafd182926d1bfbd8c1d2
Amy Chen <amysbubble@Amys-MacBook-Pro-2.local>
2022-07-28 14:11:27 -0700

    add debug statement, reformatted code

--- src/FhirClientProvider.js
+++ src/FhirClientProvider.js
@@ -1,41 +1,83 @@
-import React from 'react';
-import FHIR from 'fhirclient';
-import { FhirClientContext } from './FhirClientContext';
-import Box from '@mui/material/Box';
-import CircularProgress from '@mui/material/CircularProgress';
-import Error from './components/Error';
+import { useState, useEffect, useCallback } from "react";
+import FHIR from "fhirclient";
+import Stack from "@mui/material/Stack";
+import CircularProgress from "@mui/material/CircularProgress";
+import { FhirClientContext } from "./FhirClientContext";
+import { queryPatientIdKey } from "./util/util";
+import Error from "./components/Error";
 
-export default function FhirClientProvider (props) {
+export default function FhirClientProvider(props) {
+  const [client, setClient] = useState(null);
+  const [error, setError] = useState("");
+  const [patient, setPatient] = useState(null);
+  const [ready, setReady] = useState(false);
 
-    const [client, setClient] = React.useState(null);
-    const [error, setError] = React.useState('');
+  const getPatient = useCallback(async () => {
+    if (!client) return;
 
-    React.useEffect(() => {
-        FHIR.oauth2.ready().then(
-            (client) => setClient(client),
-            (error) => setError(error)
-        );
-    }, []);
-  
-    return (
-      <FhirClientContext.Provider value={{client:client, error: error} || {}}>
-        <FhirClientContext.Consumer>
-          {({ client, error }) => {
-            // any auth error that may have been rejected with
-            if (error) {
-              return <Error message={error.message}></Error>;
-            }
+    //this is a workaround for when patient id is NOT embedded within the JWT token
+    let queryPatientId = sessionStorage.getItem(queryPatientIdKey);
+    if (queryPatientId) {
+      console.log("Using stored patient id ", queryPatientId);
+      return client.request("/Patient/" + queryPatientId);
+    }
+    // Get the Patient resource
+    return await client.patient.read().then((pt) => {
+      return pt;
+    });
+  }, [client]);
 
-            // if client is already available render the subtree
-            if (client) {
-              return props.children;
-            }
-
-            // client is undefined until auth.ready() is fulfilled
-            return <Box><CircularProgress></CircularProgress> Authorizing...</Box>
-          }}
-        </FhirClientContext.Consumer>
-      </FhirClientContext.Provider>
+  useEffect(() => {
+    FHIR.oauth2.ready().then(
+      (client) => {
+        console.log("Auth ready..");
+        setClient(client);
+      },
+      (error) => {
+        console.log("Auth error ", error);
+        setError(error);
+      }
     );
+  }, []);
 
+  useEffect(() => {
+    if (!client) return;
+    getPatient()
+      .then((result) => {
+        setPatient(result);
+        setReady(true);
+        setError("");
+      })
+      .catch((e) => {
+        setError(e);
+      });
+  }, [client, getPatient]);
+
+  return (
+    <FhirClientContext.Provider
+      value={{ client: client, patient: patient, error: error } || {}}
+    >
+      <FhirClientContext.Consumer>
+        {({ client, error }) => {
+          // any auth error that may have been rejected with
+          if (error) {
+            return <Error message={error.message}></Error>;
+          }
+
+          // if client and patient are available render the children component(s)
+          if (!error && ready) {
+            return props.children;
+          }
+
+          // client is undefined until auth.ready() is fulfilled
+          return (
+            <Stack spacing={2} direction="row" style={{ padding: "24px" }}>
+              <CircularProgress></CircularProgress>
+              <div>Authorizing...</div>
+            </Stack>
+          );
+        }}
+      </FhirClientContext.Consumer>
+    </FhirClientContext.Provider>
+  );
 }
