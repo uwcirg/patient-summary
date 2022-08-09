d527d77f2eac6262197758e1a825ab8734210336
Amy Chen <achen2401@gmail.com>
2022-08-09 09:24:41 -0700

    read environment variables from env.json when exist (#8)
    * Add production overrides
    
    * Add config stub
    
    * Stop building after dev stage for dev override
    
    * Fixup ports for production build
    
    * Add entrypoint script to write relevant env vars to file
    
    * read environment variables from env.json when exist
    
    Co-authored-by: Ivan Cvitkovic <icvitkovic@gmail.com>
    Co-authored-by: Ivan Cvitkovic <ivanc@uw.edu>
    Co-authored-by: Amy Chen <amysbubble@Amys-MacBook-Pro-2.local>

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import {useEffect} from 'react';
 import FhirClientProvider from "../FhirClientProvider";
 import Summary from './Summary';
 import '../style/App.scss';
+import { fetchEnvData, getEnvs } from "../util/util.js";
 
 export default function App() {
+    useEffect(() => {
+        fetchEnvData();
+        console.log("environment variables ", getEnvs());
+    }, []);
     return (
         <FhirClientProvider>
             <Summary />
--- src/components/Launch.js
+++ src/components/Launch.js
@@ -3,24 +3,23 @@ import FHIR from "fhirclient";
 import Stack from "@mui/material/Stack";
 import CircularProgress from "@mui/material/CircularProgress";
 import ErrorComponent from "./ErrorComponent";
-import { getEnv, queryPatientIdKey } from "../util/util.js";
+import { fetchEnvData, getEnv, queryPatientIdKey } from "../util/util.js";
 import "../style/App.scss";
 
 export default function Launch() {
   const [error, setError] = React.useState("");
 
+  React.useEffect(() => fetchEnvData(), []);
   React.useEffect(() => {
-    let authURL = "launch-context.json";
     const backendURL = getEnv("REACT_APP_BACKEND_URL");
-    if (backendURL) {
-      authURL = `${backendURL}/auth/auth-info`;
-    }
+    const authURL = backendURL
+      ? `${backendURL}/auth/auth-info`
+      : "launch-context.json";
     const urlParams = new URLSearchParams(window.location.search);
     //retrieve patient id from URL querystring if any
-    let patientId = urlParams.get("patient");
+    const patientId = urlParams.get("patient");
     console.log("patient id from url query string: ", patientId);
     console.log("Auth url ", authURL);
-
     fetch(authURL, {
       // include cookies in request
       credentials: "include",
@@ -42,7 +41,6 @@ export default function Launch() {
         //see https://build.fhir.org/ig/HL7/smart-app-launch/scopes-and-launch-context.html
         const envAuthScopes = getEnv("REACT_APP_AUTH_SCOPES");
         if (envAuthScopes) json.scope = envAuthScopes;
-
         console.log("launch context json ", json);
         FHIR.oauth2.authorize(json).catch((e) => {
           setError(e);
