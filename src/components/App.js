c415c0cabd71c671a6399313ca0f39e83b515b42
Amy Chen <achen2401@gmail.com>
2022-09-15 12:14:31 -0700

    Bugfix/envs acess app (#15)
    * fix env access issue
    
    * remove white space
    
    * remove white space
    
    * remove extra end of file new line
    
    * add state var
    
    * remove end of line white space
    
    * fix comment
    
    * fix formatting
    
    * remove extra bracket
    
    * put old code back
    
    * fix code
    
    Co-authored-by: Amy Chen <amysbubble@Amys-MacBook-Pro-2.local>

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,15 @@
-import {useEffect} from 'react';
 import FhirClientProvider from "../FhirClientProvider";
-import Summary from './Summary';
-import '../style/App.scss';
-import { fetchEnvData, getEnvs } from "../util/util.js";
+import Summary from "./Summary";
+import "../style/App.scss";
+import {fetchEnvData, getEnvs} from "../util/util";
 
 export default function App() {
-    useEffect(() => {
-        fetchEnvData();
-        console.log("environment variables ", getEnvs());
-    }, []);
-    return (
-        <FhirClientProvider>
-            <Summary />
-            {/* add other components as needed */}
-        </FhirClientProvider>
-    );
+  fetchEnvData(); // call this to load environment variables
+  console.log("environment variables ", getEnvs());
+  return (
+    <FhirClientProvider>
+      <Summary />
+      {/* add other components as needed */}
+    </FhirClientProvider>
+  );
 }
