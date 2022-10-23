1830580988c36145a97012aae2ded5b12fed9269
Amy Chen <achen2401@gmail.com>
2022-10-23 13:06:58 -0700

    Update ScoringSummary.js

--- src/components/ScoringSummary.js
+++ src/components/ScoringSummary.js
@@ -112,7 +112,7 @@ export default function ScoringSummary(props) {
           <TableRow>
             <TableCell></TableCell>
             <TableCell variant="body">Score</TableCell>
-            <TableCell variant="body" align="right">Compared to Last</TableCell>
+            <TableCell variant="body">Compared to Last</TableCell>
           </TableRow>
           </TableHead>
           <TableBody>
