6932bf5b071f8e2e8bd4a6ff954f68bcfc8b3da1
Amy Chen <achen2401@gmail.com>
2022-10-22 18:36:25 -0700

    Fix syntax error

--- src/config/questionnaire_config.js
+++ src/config/questionnaire_config.js
@@ -20,9 +20,9 @@ const qConfig = {
     failedScores: [6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
   },
   slums: {
-    comparisonToAlert: "low", // display alert if score is lower than previous
-    failedScores: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
     scoringQuestionId: "/71492-3",
+    comparisonToAlert: "low", // display alert if score is lower than previous
+    failedScores: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]   
   },
   behav5: {
     scoringQuestionId: "behav-8",
