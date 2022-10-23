f143ce61560188dec170902b93c60c7946e63a7a
Amy Chen <achen2401@gmail.com>
2022-10-22 18:29:33 -0700

    Update questionnaire_config.js

--- src/config/questionnaire_config.js
+++ src/config/questionnaire_config.js
@@ -17,8 +17,11 @@ const qConfig = {
   },
   gds: {
     scoringQuestionId: "/48545-8",
+    failedScores: [6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
   },
   slums: {
+    comparisonToAlert: "low", // display alert if score is lower than previous
+    failedScores: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
     scoringQuestionId: "/71492-3",
   },
   behav5: {
