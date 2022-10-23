13f50493c0c9162787262b986dd6591a0bc81353
Amy Chen <achen2401@gmail.com>
2022-10-22 18:19:15 -0700

    Update questionnaire_config.js

--- src/config/questionnaire_config.js
+++ src/config/questionnaire_config.js
@@ -4,13 +4,16 @@ const qConfig = {
   minicog: {
     customCQL: true,
     scoringQuestionId: "minicog-total-score",
-    comparisonToAlert: "low" // display alert if score is lower than previous
+    comparisonToAlert: "low", // display alert if score is lower than previous
+    failedScores: [0, 1, 2]
   },
   phq9: {
     scoringQuestionId: "/44261-6",
+    failedScores: [20, 21, 22, 23, 24, 25, 26, 27]
   },
   gad7: {
     scoringQuestionId: "/70274-6",
+    failedScores: [16, 17, 18, 19, 20, 21]
   },
   gds: {
     scoringQuestionId: "/48545-8",
