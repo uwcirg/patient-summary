71f6415c94244adbc7a19f5f8508318be13bd1b9
Amy Chen <amysbubble@Amys-MacBook-Pro-3.local>
2022-11-01 11:52:48 -0700

    fix questionnaire config, add TODO's

--- src/config/questionnaire_config.js
+++ src/config/questionnaire_config.js
@@ -10,9 +10,8 @@ const qConfig = {
   },
   "c-idas": {
     scoringQuestionId: "c-idas-score",
-    // TODO, check education 21 if HS education, 20 if no HS education
     failedScores: [
-      22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36,
+      19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36,
       37, 38,
     ],
   },
