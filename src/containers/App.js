fa37f124b0b0946e840aaf3af34ebceaf88f47c5
Amy Chen <amysbubble@Amys-MacBook-Pro-3.local>
2022-11-22 09:09:20 -0800

    create new folder for container components

---
+++ src/containers/App.js
@@ -0,0 +1,12 @@
+import Summaries from "../components/Summaries";
+import TimeoutModal from "../components/TimeoutModal";
+import Base from "../components/Base";
+
+export default function App() {
+  return (
+    <Base>
+      <Summaries />
+      <TimeoutModal />
+    </Base>
+  );
+}
