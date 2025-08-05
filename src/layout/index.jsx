da66039e501233e6717d422241fb915a32edd0c9
Amy Chen <achen2401@gmail.com>
2025-08-05 14:34:11 -0700

    fix styling

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -1,12 +1,12 @@
 import PropTypes from "prop-types";
-import React, { useLayoutEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 import { QueryClient, QueryClientProvider } from "react-query";
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
 import Alert from "@mui/material/Alert";
 import AlertTitle from "@mui/material/AlertTitle";
-import { CircularProgress, Stack } from "@mui/material";
+import { CircularProgress, Stack, Typography } from "@mui/material";
 import { injectFaviconByProject, fetchEnvData } from "../util";
 import { getTheme } from "../config/theme_config";
 import "../style/App.scss";
@@ -29,7 +29,7 @@ const queryClient = new QueryClient();
 export default function Index({ children }) {
   const theme = getTheme();
   const [ready, setReady] = useState(false);
-  useLayoutEffect(() => {
+  useEffect(() => {
     if (ready) return;
     fetchEnvData().then((results) => {
       console.log("Environment variables ", results);
@@ -41,7 +41,7 @@ export default function Index({ children }) {
     return (
       <Stack spacing={2} direction="row" style={{ padding: "24px" }} alignItems="center">
         <CircularProgress></CircularProgress>
-        <div>Loading ...</div>
+        <Typography variant="body1">Loading environment variables ...</Typography>
       </Stack>
     );
   return (
