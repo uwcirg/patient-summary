03e9483f8a28fd7f87e93e7a12fc6baad50ebbbc
Amy Chen <achen2401@gmail.com>
2025-08-05 14:20:07 -0700

    update fetchEnvData to use fetch, replacing deprecated XMLHttp object

--- src/containers/LaunchContainer.jsx
+++ src/containers/LaunchContainer.jsx
@@ -25,16 +25,12 @@ const fetchContextJson = async (authURL) => {
     credentials: "include",
   }).catch((e) => {
     console.log(e);
-    throw new Error(
-      "Error retrieving context json via auth url. See console for detail."
-    );
+    throw new Error("Error retrieving context json via auth url. See console for detail.");
   });
 
   if (!response.ok) {
     console.log(response.status, response.statusText);
-    throw new Error(
-      `Error launch application: Server returned status ${response.status.toString()}`
-    );
+    throw new Error(`Error launch application: Server returned status ${response.status.toString()}`);
   }
 
   const contextJson = await response.json().catch((e) => {
@@ -47,68 +43,62 @@ const fetchContextJson = async (authURL) => {
 
 export default function Launch() {
   const [error, setError] = React.useState("");
-  fetchEnvData();
-  React.useLayoutEffect(() => {
-    const backendURL = getEnv("REACT_APP_CONF_API_URL");
-    const authURL = backendURL ? `${backendURL}/auth/auth-info` : "";
-    const urlParams = new URLSearchParams(window.location.search);
-    //retrieve patient id from URL querystring if any
-    const patientId = urlParams.get("patient");
-    console.log("patient id from url query string: ", patientId);
-    //retrieve need patient banner querystring if any
-    const needPatientBanner = urlParams.get("need_patient_banner");
-    console.log(
-      "need_patient_banner from url query string: ",
-      needPatientBanner
-    );
-    console.log("Auth url ", authURL);
-    fetchContextJson(authURL)
-      .then((json) => {
-        if (!json) {
-          setError("No valid context json specified");
-          return;
-        }
-        if (patientId) {
-          // only do this IF patient id comes from url queryString
-          json.patientId = patientId;
-          sessionStorage.setItem(queryPatientIdKey, patientId);
-        }
+  React.useEffect(() => {
+    fetchEnvData().then((results) => {
+      console.log("environment variables ", results);
+      const backendURL = getEnv("REACT_APP_CONF_API_URL");
+      const authURL = backendURL ? `${backendURL}/auth/auth-info` : "";
+      const urlParams = new URLSearchParams(window.location.search);
+      //retrieve patient id from URL querystring if any
+      const patientId = urlParams.get("patient");
+      console.log("patient id from url query string: ", patientId);
+      //retrieve need patient banner querystring if any
+      const needPatientBanner = urlParams.get("need_patient_banner");
+      console.log("need_patient_banner from url query string: ", needPatientBanner);
+      console.log("Auth url ", authURL);
+      fetchContextJson(authURL)
+        .then((json) => {
+          if (!json) {
+            setError("No valid context json specified");
+            return;
+          }
+          if (patientId) {
+            // only do this IF patient id comes from url queryString
+            json.patientId = patientId;
+            sessionStorage.setItem(queryPatientIdKey, patientId);
+          }
 
-        if (needPatientBanner !== null) {
-          json.need_patient_banner = needPatientBanner;
-          sessionStorage.setItem(queryNeedPatientBanner, needPatientBanner);
-        }
+          if (needPatientBanner !== null) {
+            json.need_patient_banner = needPatientBanner;
+            sessionStorage.setItem(queryNeedPatientBanner, needPatientBanner);
+          }
 
-        // allow client id to be configurable
-        const envClientId = getEnv("REACT_APP_CLIENT_ID");
-        if (envClientId) json.clientId = envClientId;
+          // allow client id to be configurable
+          const envClientId = getEnv("REACT_APP_CLIENT_ID");
+          if (envClientId) json.clientId = envClientId;
 
-        // allow auth scopes to be updated via environment variable
-        // see https://build.fhir.org/ig/HL7/smart-app-launch/scopes-and-launch-context.html
-        const envAuthScopes = getEnv("REACT_APP_AUTH_SCOPES");
-        if (envAuthScopes) json.scope = envAuthScopes;
+          // allow auth scopes to be updated via environment variable
+          // see https://build.fhir.org/ig/HL7/smart-app-launch/scopes-and-launch-context.html
+          const envAuthScopes = getEnv("REACT_APP_AUTH_SCOPES");
+          if (envAuthScopes) json.scope = envAuthScopes;
 
-        sessionStorage.setItem("launchContextJson", JSON.stringify(json));
+          sessionStorage.setItem("launchContextJson", JSON.stringify(json));
 
-        console.log("launch context json ", json);
-        FHIR.oauth2.authorize(json).catch((e) => {
-          console.log("FHIR auth error ", e);
-          setError("Fhir auth error. see console for detail.");
-        });
-      })
-      .catch((error) => setError(error?.message));
+          console.log("launch context json ", json);
+          FHIR.oauth2.authorize(json).catch((e) => {
+            console.log("FHIR auth error ", e);
+            setError("Fhir auth error. see console for detail.");
+          });
+        })
+        .catch((error) => setError(error?.message));
+    });
   }, []);
 
   return (
     <ThemeProvider theme={getTheme()}>
       {error && <ErrorComponent message={error}></ErrorComponent>}
       {!error && (
-        <Stack
-          spacing={2}
-          direction="row"
-          style={{ padding: "24px" }}
-          alignItems="center"
-        >
+        <Stack spacing={2} direction="row" style={{ padding: "24px" }} alignItems="center">
           <CircularProgress></CircularProgress>
           <div>Launching ...</div>
         </Stack>
