88ff9d7270e1ff81a93f141e5eb7bf581180089e
Amy Chen <achen2401@gmail.com>
2025-09-25 16:46:59 -0700

    remove conditional check for params

--- src/util/fhirUtil.js
+++ src/util/fhirUtil.js
@@ -67,12 +67,7 @@ export function getFHIRResourceTypesToLoad() {
 
 export function getFHIRResourceQueryParams(resourceType, options) {
   if (!resourceType) return null;
-  const isFromEpic = String(getEnv("REACT_APP_EPIC_QUERIES")) === "true";
-  let paramsObj = isFromEpic
-    ? {}
-    : {
-        _sort: "-_lastUpdated",
-      };
+  let paramsObj = {};
   const queryOptions = options ? options : {};
   const envCategory = getEnv("REACT_APP_FHIR_CAREPLAN_CATEGORY");
   const envObCategories = getEnv("REACT_APP_FHIR_OBSERVATION_CATEGORIES");
