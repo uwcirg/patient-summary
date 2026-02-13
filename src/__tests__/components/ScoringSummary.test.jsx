3e41b62ac042defcd9d3d27abcb50d57396be62b
Amy Chen <achen2401@gmail.com>
2026-02-13 13:06:37 -0800

    remove unnecessary mock summary data files as per feedback

--- src/__tests__/components/ScoringSummary.test.jsx
+++ src/__tests__/components/ScoringSummary.test.jsx
@@ -7,18 +7,30 @@ import { describe, it, expect } from "vitest";
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import ScoringSummary from "../../components/sections/ScoringSummary";
-import PHQ9SummaryData from "../mockfiles/MockPHQ9SummaryData.json";
-
+import questionnaireConfigs from "../../config/questionnaire_config";
+//import PHQ9SummaryData from "../mockfiles/MockPHQ9SummaryData.json";
 
 describe("Testing ScoringSummary component", () => {
   it("Renders scoring summary without data", () => {
-    const {container} = render(<ScoringSummary />);
+    const { container } = render(<ScoringSummary />);
     const noDataElement = container.querySelector(".no-data-wrapper");
     expect(noDataElement).toBeInTheDocument();
   });
 
   it("Renders score cell using data as array", () => {
-    render(<ScoringSummary data={[PHQ9SummaryData.scoringSummaryData]} />);
-    expect(screen.getByText("moderate depression")).toBeInTheDocument();
+    render(
+      <ScoringSummary
+        data={[
+          {
+            ...questionnaireConfigs["CIRG-PHQ9"],
+            score: 17,
+            scoreSeverity: "moderate",
+            date: 1712795756000,
+            tableResponseData: [{ score: 17, date: 1712795756000 }],
+          },
+        ]}
+      />,
+    );
+    expect(screen.getByText("17")).toBeInTheDocument();
   });
 });
